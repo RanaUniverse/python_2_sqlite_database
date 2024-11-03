45ce1a4bfae116d94b657eebc406f416adda8365
RanaUniverse <RanaUniverse321@gmail.com>
2024-11-03 08:53:58 +0530

    Make My Environment to use SQLMODEL

---
+++ README.md
@@ -0,0 +1,5 @@
+This is just for my practise on how to use 
+SqlModel Library
+
+
+I have Used Faker + Sqlmodel library here at beginning
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+I will learn how to use sqlite3 database system
+mainly using the SQLMODEL
+Library mainly
+Here i can just make "uv sync"
+And then i can start using sqlmodel easily
+for now here is no code
+
+"""
+
